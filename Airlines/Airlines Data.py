e49cde2180a9e5734e42ab18bf3971ac00973546
Shubham Sharma <62333773+shubhrock777@users.noreply.github.com>
2021-04-18 12:51:11 +0530

    Add files via upload

---
+++ Airlines/Airlines Data.py
@@ -0,0 +1,97 @@
+
+import pandas as pd
+import calendar
+
+
+air = pd.read_excel("D:/BLR10AM/Assi/31.forecasting- timeSeries/Datasets_Forecasting/Airlines Data.xlsx")
+
+dates = pd.date_range(start='1945-01-01', freq='MS',periods=len(air))
+
+
+
+air['Months'] = dates.month
+air['Year'] = dates.year
+
+
+air['Months'] = air['Months'].apply(lambda x: calendar.month_abbr[x])
+
+
+
+# Pre processing
+import numpy as np
+
+air["t"] = np.arange(1,97)
+
+air["t_square"] = air["t"]*air["t"]
+air["log_Passengers"] = np.log(air["Passengers"])
+air.columns
+
+    
+month_dummies = pd.DataFrame(pd.get_dummies(air['Months']))
+air1 = pd.concat([air, month_dummies], axis = 1)
+
+# Visualization - Time plot
+air1.Passengers.plot()
+
+# Data Partition
+Train = air1.head(80)
+Test = air1.tail(16)
+
+# to change the index value in pandas data frame 
+# Test.set_index(np.arange(1,13))
+
+####################### L I N E A R ##########################
+import statsmodels.formula.api as smf 
+
+linear_model = smf.ols('Passengers ~ t', data=Train).fit()
+pred_linear =  pd.Series(linear_model.predict(pd.DataFrame(Test['t'])))
+rmse_linear = np.sqrt(np.mean((np.array(Test['Passengers']) - np.array(pred_linear))**2))
+rmse_linear
+
+##################### Exponential ##############################
+
+Exp = smf.ols('log_Passengers ~ t', data = Train).fit()
+pred_Exp = pd.Series(Exp.predict(pd.DataFrame(Test['t'])))
+rmse_Exp = np.sqrt(np.mean((np.array(Test['Passengers']) - np.array(np.exp(pred_Exp)))**2))
+rmse_Exp
+
+#################### Quadratic ###############################
+
+Quad = smf.ols('Passengers ~ t+t_square', data=Train).fit()
+pred_Quad = pd.Series(Quad.predict(Test[["t","t_square"]]))
+rmse_Quad = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_Quad))**2))
+rmse_Quad
+
+################### Additive seasonality ########################
+
+add_sea = smf.ols('Passengers ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov', data=Train).fit()
+pred_add_sea = pd.Series(add_sea.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov']]))
+rmse_add_sea = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_add_sea))**2))
+rmse_add_sea
+
+################## Multiplicative Seasonality ##################
+
+Mul_sea = smf.ols('log_Passengers ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_sea = pd.Series(Mul_sea.predict(Test))
+rmse_Mult_sea = np.sqrt(np.mean((np.array(Test['Passengers']) - np.array(np.exp(pred_Mult_sea)))**2))
+rmse_Mult_sea
+
+################## Additive Seasonality Quadratic Trend ############################
+
+add_sea_Quad = smf.ols('Passengers ~ t+t_square+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data=Train).fit()
+pred_add_sea_quad = pd.Series(add_sea_Quad.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','t','t_square']]))
+rmse_add_sea_quad = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(pred_add_sea_quad))**2))
+rmse_add_sea_quad 
+
+################## Multiplicative Seasonality Linear Trend  ###########
+
+Mul_Add_sea = smf.ols('log_Passengers ~ t+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_add_sea = pd.Series(Mul_Add_sea.predict(Test))
+rmse_Mult_add_sea = np.sqrt(np.mean((np.array(Test['Passengers'])-np.array(np.exp(pred_Mult_add_sea)))**2))
+rmse_Mult_add_sea 
+
+################## Testing #######################################
+
+data = {"MODEL":pd.Series(["rmse_linear","rmse_Exp","rmse_Quad","rmse_add_sea","rmse_add_sea_quad","rmse_Mult_sea","rmse_Mult_add_sea"]),"RMSE_Values":pd.Series([rmse_linear,rmse_Exp,rmse_Quad,rmse_add_sea,rmse_add_sea_quad,rmse_Mult_sea,rmse_Mult_add_sea])}
+table_rmse=pd.DataFrame(data)
+table_rmse
