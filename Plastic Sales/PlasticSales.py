47188d4a7eecb344f086b9056bba954cbe75b3fd
Shubham Sharma <62333773+shubhrock777@users.noreply.github.com>
2021-04-20 22:58:10 +0530

    Add files via upload

---
+++ Plastic Sales/PlasticSales.py
@@ -0,0 +1,99 @@
+
+import pandas as pd
+
+
+
+sale = pd.read_csv("D:/BLR10AM/Assi/31.forecasting- timeSeries/Datasets_Forecasting/PlasticSales.csv")
+
+month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'] 
+
+# Pre processing
+import numpy as np
+
+sale["t"] = np.arange(1,61)
+
+sale["t_square"] = sale["t"]*sale["t"]
+sale["log_Sales"] = np.log(sale["Sales"])
+sale.columns
+
+
+p = sale["Month"][0]
+p[0:3]
+
+sale['months']= 0
+
+for i in range(61):
+    p = sale["Month"][i]
+    sale['months'][i]= p[0:3]
+    
+month_dummies = pd.DataFrame(pd.get_dummies(sale['months']))
+sale1 = pd.concat([sale, month_dummies], axis = 1)
+
+# Visualization - Time plot
+sale1.Sales.plot()
+
+# Data Partition
+Train = sale1.head(50)
+Test = sale1.tail(10)
+
+# to change the index value in pandas data frame 
+# Test.set_index(np.arange(1,13))
+
+####################### L I N E A R ##########################
+import statsmodels.formula.api as smf 
+
+linear_model = smf.ols('Sales ~ t', data=Train).fit()
+pred_linear =  pd.Series(linear_model.predict(pd.DataFrame(Test['t'])))
+rmse_linear = np.sqrt(np.mean((np.array(Test['Sales']) - np.array(pred_linear))**2))
+rmse_linear
+
+##################### Exponential ##############################
+
+Exp = smf.ols('log_Sales ~ t', data = Train).fit()
+pred_Exp = pd.Series(Exp.predict(pd.DataFrame(Test['t'])))
+rmse_Exp = np.sqrt(np.mean((np.array(Test['Sales']) - np.array(np.exp(pred_Exp)))**2))
+rmse_Exp
+
+#################### Quadratic ###############################
+
+Quad = smf.ols('Sales ~ t+t_square', data=Train).fit()
+pred_Quad = pd.Series(Quad.predict(Test[["t","t_square"]]))
+rmse_Quad = np.sqrt(np.mean((np.array(Test['Sales'])-np.array(pred_Quad))**2))
+rmse_Quad
+
+################### Additive seasonality ########################
+
+add_sea = smf.ols('Sales ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov', data=Train).fit()
+pred_add_sea = pd.Series(add_sea.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov']]))
+rmse_add_sea = np.sqrt(np.mean((np.array(Test['Sales'])-np.array(pred_add_sea))**2))
+rmse_add_sea
+
+################## Multiplicative Seasonality ##################
+
+Mul_sea = smf.ols('log_Sales ~ Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_sea = pd.Series(Mul_sea.predict(Test))
+rmse_Mult_sea = np.sqrt(np.mean((np.array(Test['Sales']) - np.array(np.exp(pred_Mult_sea)))**2))
+rmse_Mult_sea
+
+################## Additive Seasonality Quadratic Trend ############################
+
+add_sea_Quad = smf.ols('Sales ~ t+t_square+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data=Train).fit()
+pred_add_sea_quad = pd.Series(add_sea_Quad.predict(Test[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','t','t_square']]))
+rmse_add_sea_quad = np.sqrt(np.mean((np.array(Test['Sales'])-np.array(pred_add_sea_quad))**2))
+rmse_add_sea_quad 
+
+################## Multiplicative Seasonality Linear Trend  ###########
+
+Mul_Add_sea = smf.ols('log_Sales ~ t+Jan+Feb+Mar+Apr+May+Jun+Jul+Aug+Sep+Oct+Nov',data = Train).fit()
+pred_Mult_add_sea = pd.Series(Mul_Add_sea.predict(Test))
+rmse_Mult_add_sea = np.sqrt(np.mean((np.array(Test['Sales'])-np.array(np.exp(pred_Mult_add_sea)))**2))
+rmse_Mult_add_sea 
+
+################## Testing #######################################
+
+data = {"MODEL":pd.Series(["rmse_linear","rmse_Exp","rmse_Quad","rmse_add_sea","rmse_add_sea_quad","rmse_Mult_sea","rmse_Mult_add_sea"]),"RMSE_Values":pd.Series([rmse_linear,rmse_Exp,rmse_Quad,rmse_add_sea,rmse_add_sea_quad,rmse_Mult_sea,rmse_Mult_add_sea])}
+table_rmse=pd.DataFrame(data)
+table_rmse
+
+
+
